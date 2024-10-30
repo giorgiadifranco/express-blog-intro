419d1950081ea05da9ac6e42d41b4512ba611b34
giorgiadifranco <giorgiadf08@gmail.com>
2024-10-30 15:32:22 +0100

    add array

---
+++ data/posts.js
@@ -0,0 +1,26 @@
+const post = [{
+    titolo: '',
+    contenuto: '',
+    immagini: ,
+    tags : ['psicologia', 'neuropsicologia', 'attenzione', 'funzioni esecutive']
+}, {
+    titolo: '',
+    contenuto: '',
+    immagini: ,
+    tags : ['psicologia', 'neuropsicologia', 'attenzione', 'funzioni esecutive']
+}, {
+    titolo: '',
+    contenuto: '',
+    immagini: ,
+    tags : ['psicologia', 'neuropsicologia', 'attenzione', 'funzioni esecutive']
+}, {
+    titolo: '',
+    contenuto: '',
+    immagini: '',
+    tags : ['psicologia', 'neuropsicologia', 'attenzione', 'funzioni esecutive']
+},{
+    titolo: '',
+    contenuto: '',
+    immagini: ,
+    tags : ['psicologia', 'neuropsicologia', 'attenzione', 'funzioni esecutive']
+}]
