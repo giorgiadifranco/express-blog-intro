6bd57ca9c858447231df280dfa6eb14a7dc01e13
giorgiadifranco <giorgiadf08@gmail.com>
2024-10-30 17:25:35 +0100

    fix url img

--- data/posts.js
+++ data/posts.js
@@ -1,31 +1,31 @@
 const posts = [{
     titolo: 'Funzioni Esecutive',
     contenuto: 'cosa sono le funzioni esecutive',
-    immagini: 'https://unsplash.com/it/foto/ramo-dalbero-marrone-coperto-di-neve-bianca-h5fsy4E4FMY?utm_content=creditShareLink&utm_medium=referral&utm_source=unsplash' ,
+    immagini: 'https://images.unsplash.com/photo-1617791160536-598cf32026fb?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' ,
 
     tags : ['psicologia', 'neuropsicologia', 'attenzione', 'funzioni esecutive']
 }, {
     titolo: 'Funzioni Esecutive',
     contenuto: 'cosa sono le funzioni esecutive',
-    immagini: 'https://unsplash.com/it/foto/ramo-dalbero-marrone-coperto-di-neve-bianca-h5fsy4E4FMY?utm_content=creditShareLink&utm_medium=referral&utm_source=unsplash' ,
+    immagini: 'https://images.unsplash.com/photo-1559757175-5700dde675bc?q=80&w=1931&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' ,
 
     tags : ['psicologia', 'neuropsicologia', 'attenzione', 'funzioni esecutive']
 },{
     titolo: 'Funzioni Esecutive',
     contenuto: 'cosa sono le funzioni esecutive',
-    immagini: 'https://unsplash.com/it/foto/ramo-dalbero-marrone-coperto-di-neve-bianca-h5fsy4E4FMY?utm_content=creditShareLink&utm_medium=referral&utm_source=unsplash' ,
+    immagini: 'https://images.unsplash.com/photo-1566669437687-7040a6926753?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' ,
 
     tags : ['psicologia', 'neuropsicologia', 'attenzione', 'funzioni esecutive']
 }, {
     titolo: 'Funzioni Esecutive',
     contenuto: 'cosa sono le funzioni esecutive',
-    immagini: 'https://unsplash.com/it/foto/ramo-dalbero-marrone-coperto-di-neve-bianca-h5fsy4E4FMY?utm_content=creditShareLink&utm_medium=referral&utm_source=unsplash' ,
+    immagini: 'https://images.unsplash.com/photo-1526500436895-70417935a42f?q=80&w=1889&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' ,
 
     tags : ['psicologia', 'neuropsicologia', 'attenzione', 'funzioni esecutive']
 },{
     titolo: 'Funzioni Esecutive',
     contenuto: 'cosa sono le funzioni esecutive',
-    immagini: 'https://unsplash.com/it/foto/ramo-dalbero-marrone-coperto-di-neve-bianca-h5fsy4E4FMY?utm_content=creditShareLink&utm_medium=referral&utm_source=unsplash' ,
+    immagini: 'https://images.unsplash.com/photo-1526578544885-99bb85b5b5d0?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' ,
 
     tags : ['psicologia', 'neuropsicologia', 'attenzione', 'funzioni esecutive']
 },]
